31294f1ac208f2bda2ca4c0c5fbfbbaa32f970ba
jingweimo <jingweimo89@gmail.com>
2018-03-22 18:18:20 -0400

    Add files via upload

---
+++ ch6 demo.R
@@ -0,0 +1,37 @@
+# Table 6.1 dataset
+x<-c(1089,1092,rep(1094,2),rep(1095,4),rep(1098,8),rep(1100,9),rep(1104,12),rep(1105,4),rep(1107,5),rep(1108,5),rep(1110,4),rep(1112,3),rep(1115,2))
+
+# histogram visualization: Figure 6.1
+breaks <- seq(1085,1120,5)
+hist(x, breaks = breaks, freq=FALSE, right = FALSE, xlab="Temperature range",ylab="Number of measurements",main="Histogram")
+# you can a distribution curve 
+#curve(dnorm(x, mean=mean(x), sd=sd(x)), add=TRUE, col="darkblue", lwd=2)  
+
+# calculate statistics
+mean(x)
+getmode(x)
+median(x)
+var(x)
+sd(x)
+sd(x)/mean(x) #coefficient of variation
+sd(x)/length(x) #standard error
+
+#user function: getmode()
+getmode <- function(v) {
+  uniqv <- unique(v)
+  uniqv[which.max(tabulate(match(v, uniqv)))]
+}
+
+#uncertainty of the mean
+qnorm(p=0.975)*sd(x)/length(x)
+#---------------------------------------------------------------------------------------
+#Introduction to R: 
+# 1). help(), built-in functions
+# 2). install.packages(), take for example "asbio" and "chemometrics" 
+# 3). ctrl+L or cat('\f')
+# 4). Online-resources: https://www.rdocumentation.org
+Mode(x) # "asbio" function
+
+#---------------------------------------------------------------------------------------
+
+#Multivaraite 
