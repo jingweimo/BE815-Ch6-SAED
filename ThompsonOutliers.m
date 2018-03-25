9950e92679864a814f1e96869ad48596cc45c612
jingweimo <jingweimo89@gmail.com>
2018-03-25 12:32:14 -0400

    Add files via upload

--- ThompsonOutliers.m
+++ ThompsonOutliers.m
@@ -3,9 +3,10 @@ function out = ThompsonOutliers(in)
 %YL 03/25/2018
 
 %Thompson's value
-t = [1.15,1.393,1.572,1.656,1.711,1.749,1.777,1.798,1.815,1.829,1.840,1.849,...
+tau = [1.15,1.393,1.572,1.656,1.711,1.749,1.777,1.798,1.815,1.829,1.840,1.849,...
     1.858,1.865,1.871,1.876,1.881,1.885,1.889,1.893,1.896,1.899,1.902,1.904,1.906,1.908,...
     1.910,1.911,1.913,1.914,1.916,1.917,1.919,1.920,1.921,1.922,1.923,1.924];
+%ot using the analytical formula for the tau values
 
 out = [];
 N = length(in);
@@ -14,14 +15,14 @@ if N>=3 && N<=40
     while ~done
         mu = mean(in);
         sd = std(in);
-        temp = abs(in-mu)>t(N-2)*sd;
-        tempIdx = find(temp==1);
-        out = [out, in(temp)];
-        if ~isempty(tempIdx)
-            in(tempIdx) = [];
-            if length(in)<3
+        ind = find(abs(in-mu)==max(abs(in-mu)),1); %one at a time
+        if abs(in(ind)-mu)>tau(N-2)*sd
+            out = [out, in(ind)];
+            in(ind) = [];
+            N = length(in);
+            if N<3
                 break;
-            end
+            end           
         else
             done = true;
         end
